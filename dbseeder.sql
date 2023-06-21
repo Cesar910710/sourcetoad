02ab652a4f7fb965ad2cb14b05f1c2c6bbd6c248
Cesar910710 <cesar910710@gmail.com>
2023-06-20 23:27:25 -0500

    Create dbseeder.sql

---
+++ dbseeder.sql
@@ -0,0 +1,26 @@
+    INSERT INTO customers(first_name, last_name)
+    VALUES('Customer','One');
+
+    INSERT INTO customers(first_name, last_name)
+    VALUES('Customer','Two');
+
+    INSERT INTO customers(first_name, last_name)
+    VALUES('Customer','Three');
+
+    INSERT INTO items(name, quantity, price)
+    VALUES('item1',33,3000);
+
+    INSERT INTO items(name, quantity, price)
+    VALUES('item2',66,6000);
+
+    INSERT INTO items(name, quantity, price)
+    VALUES('item3',99,9000);
+
+    INSERT INTO addresses(line_1, city, state, zip, customer)
+    VALUES('line one','Cali','Valle', 'zipi1', 1);
+
+    INSERT INTO addresses(line_1, city, state, zip, customer)
+    VALUES('line two','Medellin','Antioquia', 'zipi2', 2);
+
+    INSERT INTO addresses(line_1, city, state, zip, customer)
+    VALUES('line three','Cartagena','Bolivar', 'zipi3', 3);
